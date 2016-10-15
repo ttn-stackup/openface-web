046be3c011b0067b39c8a11378149e11612ac771
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-10-15 16:27:30 +0800

    Updated path

--- start-servers.sh
+++ start-servers.sh
@@ -27,6 +27,6 @@ WEBSOCKET_LOG='/tmp/openface.websocket.log'
 printf "WebSocket Server: Logging to '%s'\n\n" $WEBSOCKET_LOG
 
 cd ../../ # Root OpenFace directory.
-./demos/web/websocket-server.py --port $WEBSOCKET_PORT 2>&1 | tee $WEBSOCKET_LOG &
+./websocket-server.py --port $WEBSOCKET_PORT 2>&1 | tee $WEBSOCKET_LOG &
 
 wait
